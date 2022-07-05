fd52b278f1aad25a6072850db83a34ca8134219a
Robin <robin_0695@hotmail.com>
2022-07-05 18:01:24 +0800

    update

---
+++ course/snake-game.py
@@ -0,0 +1,154 @@
+import sys
+import pygame
+import random
+
+# 变量初始化
+# snake的颜色
+green = (0, 155, 0)
+black = (0, 0, 0)
+red = (255, 0, 0)
+white = (255, 255, 255)
+
+# snake block size
+block_size = 10
+
+# window size 初始化
+display_width = 400
+display_height = 600
+
+# 初始化pygame，包括时钟，字体等
+pygame.init()
+pygame.display.set_caption("My Snake Game")
+
+game_display = pygame.display.set_mode((display_width, display_height))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 20)
+
+# ---------------------------------------------------------------------
+# 定义蛇的身体和节点的数据结构
+# unit = [x, y] 节点的坐标。
+# snake boday = [unit, unit ......]
+snake_unit = []
+snake_body = []
+snake_body_size = 10
+# 初始化蛇的身体
+for i in range(0, snake_body_size) :
+    snake_unit = [20 + i * block_size, 300]
+    snake_body.append(snake_unit)
+# ---------------------------------------------------------------------
+
+# 移动的方向和数值
+move_step = [block_size, 0]
+
+# food
+isEaten = True
+food_x = 0
+food_y = 0
+
+# 游戏是否结束
+is_finished = False
+
+# 信息， 分数
+msg_from_game = "Running..."
+score = 0
+level = 0
+speed_up = 0
+# main loop，主循环
+while True:
+    
+    # 画布填充黑色
+    game_display.fill(black)
+    
+    # 设置时钟
+    clock.tick(20 + level * 5 + speed_up)
+
+    # 每次获取蛇的长度
+    snake_body_size = len(snake_body)
+    snake_head = snake_body[snake_body_size - 1]
+    snake_head_new = [snake_head[0] + move_step[0], snake_head[1] + move_step[1]]
+
+    if (snake_head[0] <= 0 or snake_head[0] >= display_width) or (snake_head[1] <= 0 or snake_head[1] >= display_height - 80):
+        is_finished = True
+        msg_from_game = "Good play, game over!"
+        
+    # 添加关闭窗口事件处理
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("Close the window!")
+            pygame.quit()
+            sys.exit(0)
+
+        # 处理键盘上下左右的事件，移动snake
+        if event.type == pygame.KEYDOWN:
+            if event.key in (pygame.K_DOWN, pygame.K_UP, pygame.K_LEFT, pygame.K_RIGHT):
+                if event.key == pygame.K_DOWN:
+                    move_step = [0, block_size]
+                if event.key == pygame.K_UP:
+                    move_step = [0, - block_size]
+                if event.key == pygame.K_LEFT:
+                    move_step = [-block_size, 0]
+                if event.key == pygame.K_RIGHT:
+                    move_step = [block_size, 0]
+                # 增加刷新速度
+                speed_up = 20
+                print(snake_body)
+
+            if event.key == pygame.K_q:
+                pygame.quit()
+                sys.exit(0)
+            
+            if event.key == pygame.K_SPACE:
+                # 重置游戏变量
+                snake_unit = []
+                snake_body = []
+                snake_body_size = 10
+                for i in range(0, snake_body_size) :
+                    snake_unit = [20 + i * block_size, 300]
+                    snake_body.append(snake_unit)
+                move_step = [block_size, 0]
+                isEaten = True
+                is_finished = False
+                msg_from_game = "Running..."
+                score = 0
+                level = 0
+                snake_head = snake_body[snake_body_size - 1]
+                snake_head_new = [snake_head[0] + move_step[0], snake_head[1] + move_step[1]]
+            
+        # 当按键抬起取消加速效果，只处理上下左右
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_DOWN, pygame.K_UP, pygame.K_LEFT, pygame.K_RIGHT):
+                speed_up = 0
+                
+    if not is_finished:
+        snake_body.append(snake_head_new)
+
+        # 画出食物
+        if isEaten:
+            food_x = round(random.randrange(0, display_width - block_size - 75) / 10) * 10
+            food_y = round(random.randrange(0, display_height - block_size - 75) / 10) * 10
+            isEaten = False
+            
+        if food_x == snake_head_new[0] and food_y == snake_head_new[1]:
+            isEaten = True
+            score += 10
+            level = int(score / 100)
+        else:
+            del(snake_body[0])
+
+    # 通过draw 矩形，形成蛇的身体 
+    for i in snake_body:
+        pygame.draw.rect(game_display, green, [i[0], i[1], block_size, block_size])
+    pygame.draw.rect(game_display, red, [food_x, food_y, block_size, block_size])
+
+    pygame.draw.line(game_display, white, [0, display_height - 75], [display_width, display_height - 75], 2)
+    
+    msg = font.render(f"MESSAGE:{msg_from_game}", True, white)
+    info = font.render(f"SCORE:{score} | LEVEL:{level + 1}| X:{snake_head[0]} Y:{snake_head[1]}", True, white)
+    help_message = font.render(f"Press Q to quite, SPACE to replay", True, white)
+    game_display.blit(msg, [10, display_height - 25])
+    game_display.blit(info, [10, display_height - 45])
+    game_display.blit(help_message, [10, display_height - 65])
+
+    # 更新画布，并翻页
+    pygame.display.update()
+    pygame.display.flip()
---
+++ course/snake-game_oop.py
@@ -0,0 +1,206 @@
+
+import sys
+import pygame
+import random
+
+GREEN = (0, 155, 0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+# window size 初始化
+DISPLAY_WIDTH = 400
+DISPLAY_HEIGHT = 600
+
+class Snake:
+    def __init__(self) -> None:
+        self.block_size = 10
+        snake_unit = []
+        snake_body_size = 10
+        self.snake_body = []
+        # 初始化蛇的身体
+        for i in range(0, snake_body_size) :
+            snake_unit = [20 + i * self.block_size, 300]
+            self.snake_body.append(snake_unit)
+
+        food_music = 'music.mp3'
+        self.eat_food_sound = pygame.mixer.Sound(food_music)
+
+    def move(self, move_step, eat_food):
+        # 每次获取蛇的长度
+        snake_body_size = len(self.snake_body)
+        snake_head = self.snake_body[snake_body_size - 1]
+        snake_head_new = [snake_head[0] + move_step[0], snake_head[1] + move_step[1]]
+        self.snake_body.append(snake_head_new)
+        if not eat_food:
+            del(self.snake_body[0])
+        else:
+            self.eat_food_sound.play()
+        return self.snake_body
+    
+    def get_snake_body(self):
+        return self.snake_body
+    
+    def get_snake_unit_size(self):
+        return self.block_size
+
+    def get_snake_size(self):
+        return len(self.snake_body)
+
+    def get_snake_head(self):
+        return self.snake_body[len(self.snake_body) - 1]
+    
+    def draw(self, game_display):
+        # 通过draw 矩形，形成蛇的身体 
+        for i in self.snake_body:
+            pygame.draw.rect(game_display, GREEN, [i[0], i[1], self.block_size, self.block_size])
+        
+        
+class Food:
+    def __init__(self, x, y, basic_color) -> None:
+        self.block_size = 10
+        self.x = x
+        self.y = y
+        self.count = 0
+        self.basic_color = basic_color
+        self.food_color_incr = 255
+        food_cod_temp = self.__gen_cod()
+        self.food_cod = [food_cod_temp[0], food_cod_temp[1]]
+
+    def __gen_cod(self):
+        food_x = round(random.randrange(0, self.x - self.block_size - 75) / 10) * 10
+        food_y = round(random.randrange(0, self.y - self.block_size - 75) / 10) * 10
+        return [food_x, food_y]
+
+    def refresh(self):
+        food_cod_temp = self.__gen_cod()
+        self.food_cod = [food_cod_temp[0], food_cod_temp[1]]
+        return self.food_cod
+        
+    def food_eaten(self):
+        self.count += 1
+        self.refresh()
+
+    def get_food_count(self):
+        return self.count
+
+    def get_food_cod(self):
+        return self.food_cod
+    
+    def draw(self, game_display):
+        self.food_color_incr -= 10
+        if self.food_color_incr <= 0:
+            self.food_color_incr = 255
+        pygame.draw.rect(game_display, (self.basic_color, self.food_color_incr, self.food_color_incr),
+                         [self.food_cod[0], self.food_cod[1], self.block_size, self.block_size])
+        
+
+class GameInfo:
+    def __init__(self, y) -> None:
+        self.y = y
+
+    def draw(self, game_display, font, score, level, snake_head, message):
+        pygame.draw.line(game_display, WHITE, [0, DISPLAY_HEIGHT - self.y], [DISPLAY_WIDTH, DISPLAY_HEIGHT - self.y], 2)
+        msg = font.render(f"MESSAGE:{message}", True, WHITE)
+        info = font.render(f"SCORE:{score} | LEVEL:{level + 1}| X:{snake_head[0]} Y:{snake_head[1]}", True, WHITE)
+        help_message = font.render(f"Press Q to quite, SPACE to replay", True, WHITE)
+        game_display.blit(msg, [10, DISPLAY_HEIGHT - (self.y - 50)])
+        game_display.blit(info, [10, DISPLAY_HEIGHT - (self.y - 30)])
+        game_display.blit(help_message, [10, DISPLAY_HEIGHT - (self.y - 10)])
+
+
+class GameManager:
+    def __init__(self, game_display) -> None:
+        self.snake = Snake()
+        self.food = Food(DISPLAY_WIDTH, DISPLAY_HEIGHT, 255)
+        self.game_display = game_display
+        self.move_step = [self.snake.get_snake_unit_size(), 0]
+        self.game_info = GameInfo(75)
+        self.font = pygame.font.SysFont(None, 20)
+    
+    def work(self):
+        if not self.check_game_over():
+            message = "Running...."
+            if self.check_eat_food() :
+                self.snake.move(self.move_step, True)
+                self.food.food_eaten()
+            else:
+                self.snake.move(self.move_step, False)
+
+            self.food.draw(self.game_display)
+
+        else:
+            message = "Good play, game over!"
+        self.snake.draw(self.game_display)
+        self.game_info.draw(self.game_display, self.font, self.food.get_food_count() * 10, 
+                            int(self.food.get_food_count() / 10), self.snake.get_snake_head(), message)
+    
+    def handle_key_down(self, key):
+        if key in (pygame.K_DOWN, pygame.K_UP, pygame.K_LEFT, pygame.K_RIGHT):
+            if key == pygame.K_DOWN:
+                move_step = [0, self.snake.get_snake_unit_size()]
+            if key == pygame.K_UP:
+                move_step = [0, - self.snake.get_snake_unit_size()]
+            if key == pygame.K_LEFT:
+                move_step = [-self.snake.get_snake_unit_size(), 0]
+            if key == pygame.K_RIGHT:
+                move_step = [self.snake.get_snake_unit_size(), 0]
+            self.move_step = move_step
+
+    def check_eat_food(self):
+        if  self.snake.get_snake_head()[0] == self.food.get_food_cod()[0] and \
+            self.snake.get_snake_head()[1] == self.food.get_food_cod()[1]:
+            return True
+        else:
+            return False
+            
+    def check_game_over(self):
+        if  (self.snake.get_snake_head()[0] <= 0 or self.snake.get_snake_head()[0] >= DISPLAY_WIDTH) or \
+            (self.snake.get_snake_head()[1] <= 0 or self.snake.get_snake_head()[1] >= DISPLAY_HEIGHT - 80):
+            return True
+
+class Game:
+    def __init__(self) -> None:
+        # 初始化pygame，包括时钟，字体等
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption("My Snake Game")
+        self.game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.gameManager = GameManager(self.game_display)
+
+        bg_img = pygame.image.load('bg.png')
+        self.bg_image = pygame.transform.scale(bg_img, (400, 525))
+
+        back_music = 'background.mp3'
+        back_music_sound = pygame.mixer.Sound(back_music)
+        back_music_sound.play(-1)
+    
+    def play(self):
+        while True:
+            self.clock.tick(20)
+            self.game_display.fill(BLACK)
+            self.game_display.blit(self.bg_image, (0, 0))
+            # 添加关闭窗口事件处理
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print("Close the window!")
+                    pygame.quit()
+                    sys.exit(0)
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.gameManager = GameManager(self.game_display)
+                    elif event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit(0)
+                    else:
+                        self.gameManager.handle_key_down(event.key)
+                
+            # 更新画布，并翻页
+            self.gameManager.work()
+            pygame.display.update()
+            pygame.display.flip()
+
+game = Game()
+game.play()
